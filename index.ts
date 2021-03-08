73202970f1cc8c99ec259a0325de5a9c8febb1fd
Guohui Xiao <xiao@inf.unibz.it>
2021-03-08 15:35:37 +0100

    add type annotations

--- index.ts
+++ index.ts
@@ -1,45 +1,50 @@
 console.clear();
-import { interval, fromEvent, combineLatest, of, BehaviorSubject, noop } from 'rxjs';
+import {interval, fromEvent, combineLatest, of, BehaviorSubject, noop, Observable} from 'rxjs';
 import { scan, tap, pluck, startWith, takeWhile, finalize, switchMap } from 'rxjs/operators';
-import { Car, Road, Player, Game } from './interfaces';
+import {Car, Road, Player, Game, State} from './interfaces';
 import { gameHeight, gameWidth, levelDuration } from './constants';
 import { updateState } from './state';
 import { render, renderGameOver } from './html-renderer';
 
 const car = (x: number, y: number): Car => ({ x, y, scored: false });
-const randomCar = (): Car => car(0, Math.floor(Math.random() * Math.floor(gameWidth)));
-const gameSpeed$ = new BehaviorSubject(200);
 
-const road$ = gameSpeed$.pipe(
+const randomCar = (): Car => car(0, Math.floor(Math.random() * Math.floor(gameWidth)));
+
+const gameSpeed$: BehaviorSubject<number> = new BehaviorSubject(200);
+
+const road$: Observable<Road> = gameSpeed$.pipe(
   switchMap(i =>
     interval(i)
       .pipe(
-        scan((road: Road, _: number): Road => (
-          road.cars = road.cars.filter(c => c.x < gameHeight - 1),
-          road.cars[0].x === (gameHeight / 2) ? road.cars.push(randomCar()) : noop,
-          road.cars.forEach(c => c.x++),
-          road
-        ), { cars: [randomCar()] })
+        scan((road: Road, _: number): Road => {
+          road.cars = road.cars.filter(c => c.x < gameHeight - 1);
+          road.cars[0].x === (gameHeight / 2) ? road.cars.push(randomCar()) : noop;
+          road.cars.forEach(c => c.x++);
+          return road
+        }, { cars: [randomCar()] })
       )
   ));
 
-const keys$ = fromEvent(document, 'keyup')
+const keys$: Observable<string> = fromEvent(document, 'keyup')
   .pipe(
     startWith({ code: '' }),
     pluck('code')
   );
 
-const player$ = keys$
+const player$: Observable<Player> = keys$
   .pipe(
-    scan((player: Player, key: string): Player => (player.y +=
-      key === 'ArrowLeft' && player.y > 0
-        ? -1
-        : key === 'ArrowRight' && player.y < gameWidth - 1
-          ? 1
-          : 0, player), { y: 0 })
+    scan((player: Player, key: string): Player => {
+        player.y +=
+            key === 'ArrowLeft' && player.y > 0
+                ? -1
+            : key === 'ArrowRight' && player.y < gameWidth - 1
+                ? 1
+            : 0;
+        return player}
+    , { y: 0 })
   );
 
-const state$ = of({
+const state$: Observable<State> = of({
   score: 1,
   lives: 3,
   level: 1,
